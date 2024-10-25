3689f724ea6d63633615f37add8ade93629644d8
Andiy <andriemeheden@gmail.com>
2024-10-25 19:02:17 +0300

    1

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: true,
-  remoteApi: 'https://backend-be-best.vercel.app/api',
+  remoteApi: 'backend-be-best-4poy.vercel.app',
   // socketNotificationApi: 'http://localhost:8000/notification/socket.io',
   socketConfigApi: 'http://localhost:8001/config/socket.io',
   wordpressApi: 'https://content.bebest.petrishin.com/index.php/wp-json/wp/v2',
